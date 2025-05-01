30be4a25ae3c6150de40986b3d7e0e2106246b98
Kasun Lakshitha <kasun2014@outlook.com>
2025-05-01 14:14:52 +0530

    deleted all files

--- app/(protected)/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-	return (
-		<div className='p-4'>
-			<h1 className='text-2xl font-semibold'>Dashboard</h1>
-			<p>Welcome to your protected dashboard!</p>
-		</div>
-	);
-}
